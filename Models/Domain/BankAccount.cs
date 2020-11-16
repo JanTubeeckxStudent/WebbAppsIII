f996f6ec4366d7e872b9e43da5afa404b51322a7
jantubeeckx <jan.tubeeckx@student.hogent.be>
2020-11-16 22:13:04 +0100

    Add class BankAccount

---
+++ Models/Domain/BankAccount.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Banking.Models.Domain
+{
+    public class BankAccount
+    {
+        private readonly string _accountNumber;
+        private decimal _balance;
+        private const decimal WithdrawCost = 0.25M;
+
+    }
+}
