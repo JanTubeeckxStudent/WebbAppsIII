279462912725ae125266ca2932b758e94ae54cf0
jantubeeckx <jan.tubeeckx@student.hogent.be>
2020-11-16 22:21:29 +0100

    add first method in BankAccount

--- Models/Domain/BankAccount.cs
+++ Models/Domain/BankAccount.cs
@@ -7,6 +7,9 @@ namespace Banking.Models.Domain
         private decimal _balance;
         private const decimal WithdrawCost = 0.25M;
 
-
+        public void Deposit(decimal amount)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
