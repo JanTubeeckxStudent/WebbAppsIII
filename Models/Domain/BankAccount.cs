79db1b102b32e5ad515943f78e0883ea2197af2d
jantubeeckx <jan.tubeeckx@student.hogent.be>
2020-11-16 22:40:35 +0100

    Complete class BankAccount

--- Models/Domain/BankAccount.cs
+++ Models/Domain/BankAccount.cs
@@ -3,13 +3,37 @@ namespace Banking.Models.Domain
 {
     public class BankAccount
     {
+        #region Fields
         private readonly string _accountNumber;
         private decimal _balance;
         private const decimal WithdrawCost = 0.25M;
+        #endregion
 
+        #region Properties
+        public string AccountNumber { get; }
+
+        public decimal Balance { get; private set; }
+        #endregion
+
+        #region Constructor
+        public BankAccount(string account)
+        {
+            AccountNumber = account;
+            Balance = Decimal.Zero;
+        }
+        #endregion
+
+        #region Methods
         public void Deposit(decimal amount)
         {
-            throw new NotImplementedException();
+            Balance += amount;
         }
+
+        public void Withdraw(decimal amount)
+        {
+            Balance -= amount;
+        }
+        #endregion
+
     }
 }
